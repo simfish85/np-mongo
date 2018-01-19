45335a881bf7814f3a5247f433ce48282c87de6d
Simon Fisher <simfish85@googlemail.com>
2018-01-19 15:36:29 +0000

    Added ssh to plan.sh

--- plan.sh
+++ plan.sh
@@ -1,10 +1,10 @@
 pkg_origin=simfish85
 pkg_name=np-mongodb
-pkg_version=3.2.9
+pkg_version=3.2.10
 pkg_description="Mongodb for National-Parks app"
 pkg_maintainer="echohack"
 pkg_license=('AGPL-3.0')
-pkg_deps=(core/mongodb core/mongo-tools)
+pkg_deps=(core/mongodb core/mongo-tools core/openssh)
 pkg_svc_run="mongod --config $pkg_svc_config_path/mongod.conf"
 pkg_svc_user=hab
 pkg_svc_group=hab
